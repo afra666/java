5968131ac8b7e69d035f16d011deda2c9c9b2b92
chooseit <48674984+chooseit@users.noreply.github.com>
2019-03-22 09:55:05 +0800

    Update and rename 1.StringBuilder to 1.StringBuilder.java

---
+++ 1.StringBuilder.java
@@ -0,0 +1,23 @@
+package p;
+import java.util.Scanner;
+public class Test {
+	public static void main(String[] args) {
+		StringBuilder str1=new StringBuilder();
+		str1.append("a");
+		str1.append("b");
+		System.out.println(str1);
+	}
+}
+
+/**************************************************/
+
+package p;
+import java.util.Scanner;
+public class Test {
+	public static void main(String[] args) {
+		StringBuilder str1=new StringBuilder();
+		str1.insert(0, "12345678");
+		str1.insert(2, "abc");
+		System.out.println(str1);
+	}
+}
