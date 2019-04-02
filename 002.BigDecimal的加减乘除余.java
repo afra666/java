e33b89f147fb8d1a4bf0597ddbd9b45e36726a3b
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-02 11:08:42 +0800

    Create 002.BigDecimal的加减乘除余.java

---
+++ 002.BigDecimal的加减乘除余.java
@@ -0,0 +1,78 @@
+/**
+ * BigDecimal的输入输出
+ */
+package p;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.math.BigDecimal;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		BigDecimal one=new BigDecimal(1);
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("请输入b1:");
+		BigDecimal b1=scanner.nextBigDecimal();
+		System.out.println("请输入b2:");
+		BigDecimal b2=scanner.nextBigDecimal();
+		//BigDecimal b2=new BigDecimal(3);	
+		System.out.println("请输入结果保留的精度");
+		int n=scanner.nextInt();
+		scanner.close();
+		BigDecimal add,subtract,multiply,divide,remainder;//加减乘除余
+		
+		//加减乘除余赋值
+		add           = b1.add(b2)    ;
+		subtract      = b1.subtract(b2)   ;
+		multiply      = b1.multiply(b2)   ;
+		divide        = b1.divide(b2,n,RoundingMode.HALF_UP)   ;
+		remainder     = b1.remainder(b2)   ;
+		
+		//输出
+		System.out.println("/===========================================/");
+		System.out.println("===b1="+b1);
+		System.out.println("b1的正负sign判断："+b1.signum());
+		//System.out.println("b1的标度scale判断："+b1.scale());
+		System.out.println("b1去尾数零后标度scale判断："+b1.stripTrailingZeros().scale());
+		System.out.println("===b2="+b2);
+		System.out.println("b2的正负sign判断："+b2.signum());
+		//System.out.println("b2的标度scale判断："+b2.scale());
+		System.out.println("b2去尾数零后标度scale判断："+b2.stripTrailingZeros().scale());
+		System.out.println("/===========================================/");
+		System.out.println("b1+b2="+add);
+		System.out.println("b1-b2="+subtract);
+		System.out.println("b1*b2="+multiply);
+		System.out.println("(四舍五入)\nb1/b2="+divide);
+		if ((remainder).scale()==0) {
+			System.out.println("b1%b2="+remainder);
+		}
+		else {
+			System.out.println("b1%b2=不存在");
+		}
+		
+	}
+
+}
+/**
+请输入b1:
+2
+请输入b2:
+1
+请输入结果保留的精度
+8
+/===========================================/
+===b1=2
+b1的正负sign判断：1
+b1去尾数零后标度scale判断：0
+===b2=1
+b2的正负sign判断：1
+b2去尾数零后标度scale判断：0
+/===========================================/
+b1+b2=3
+b1-b2=1
+b1*b2=2
+(四舍五入)
+b1/b2=2.00000000
+b1%b2=0
+
+*/
