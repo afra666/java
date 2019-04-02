8ba92f64063f1cecdfd4fba7f455bb2a4855fd4a
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-02 11:30:16 +0800

    Update 002.BigDecimal的加减乘除余.java

--- 002.BigDecimal的加减乘除余.java
+++ 002.BigDecimal的加减乘除余.java
@@ -16,8 +16,11 @@ public class Main {
 		System.out.println("请输入b2:");
 		BigDecimal b2=scanner.nextBigDecimal();
 		//BigDecimal b2=new BigDecimal(3);	
-		System.out.println("请输入结果保留的精度");
-		int n=scanner.nextInt();
+		
+		int n=Math.max(
+				(b1.stripTrailingZeros().scale()),
+				(b2.stripTrailingZeros().scale())
+				);
 		scanner.close();
 		BigDecimal add,subtract,multiply,divide,remainder;//加减乘除余
 		
@@ -29,21 +32,22 @@ public class Main {
 		remainder     = b1.remainder(b2)   ;
 		
 		//输出
-		System.out.println("/===========================================/");
-		System.out.println("===b1="+b1);
-		System.out.println("b1的正负sign判断："+b1.signum());
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("最长精度为：                                                                  "+n);
+		System.out.println("###b1="+b1);
+		System.out.println("b1的正负sign判断：\n"+b1.signum());
 		//System.out.println("b1的标度scale判断："+b1.scale());
-		System.out.println("b1去尾数零后标度scale判断："+b1.stripTrailingZeros().scale());
-		System.out.println("===b2="+b2);
+		System.out.println("b1去尾数零后标度scale判断：\n"+b1.stripTrailingZeros().scale());
+		System.out.println("###b2="+b2);
 		System.out.println("b2的正负sign判断："+b2.signum());
 		//System.out.println("b2的标度scale判断："+b2.scale());
 		System.out.println("b2去尾数零后标度scale判断："+b2.stripTrailingZeros().scale());
-		System.out.println("/===========================================/");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		System.out.println("b1+b2="+add);
 		System.out.println("b1-b2="+subtract);
 		System.out.println("b1*b2="+multiply);
 		System.out.println("(四舍五入)\nb1/b2="+divide);
-		if ((remainder).scale()==0) {
+		if ((b1.stripTrailingZeros().scale()==0)&&(b2.stripTrailingZeros().scale()==0)) {
 			System.out.println("b1%b2="+remainder);
 		}
 		else {
@@ -55,24 +59,25 @@ public class Main {
 }
 /**
 请输入b1:
-2
+2.1
 请输入b2:
 1
-请输入结果保留的精度
-8
-/===========================================/
-===b1=2
-b1的正负sign判断：1
-b1去尾数零后标度scale判断：0
-===b2=1
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+最长精度为：                                                                  1
+###b1=2.1
+b1的正负sign判断：
+1
+b1去尾数零后标度scale判断：
+1
+###b2=1
 b2的正负sign判断：1
 b2去尾数零后标度scale判断：0
-/===========================================/
-b1+b2=3
-b1-b2=1
-b1*b2=2
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+b1+b2=3.1
+b1-b2=1.1
+b1*b2=2.1
 (四舍五入)
-b1/b2=2.00000000
-b1%b2=0
+b1/b2=2.1
+b1%b2=不存在
 
 */
