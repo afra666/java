bf657ba535e9b9ac26c647c08982230b0f82438f
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-04 10:54:11 +0800

    Update 019.String转BigDecimal.java

--- 019.String转BigDecimal.java
+++ 019.String转BigDecimal.java
@@ -23,3 +23,21 @@ public class Main {
 bd1=3.14156
 输入格式有误
 */
+
+
+/**
+ * BigDecimal转String
+ */
+package p;
+import java.math.BigDecimal;
+public class Main {
+	public static void main(String[] args) {
+		BigDecimal bd1=new BigDecimal("3.141592");//不加引号会输出很多位
+		String str1=null;
+		System.out.println("str1="+bd1.toString());
+		}
+}
+
+/**输出
+str1=3.141592
+*/
