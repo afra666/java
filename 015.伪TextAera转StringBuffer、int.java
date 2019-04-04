a8a784fa351303549f5f82a8a48617d6b74d5460
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-04 08:44:51 +0800

    Create 015.伪TextAera转StringBuffer、int.java

---
+++ 015.伪TextAera转StringBuffer、int.java
@@ -0,0 +1,13 @@
+/**
+ * TextArea >>>>>StringBuffer
+ */
+StringBuffer strbf2=new StringBuffer();
+strbf2=new StringBuffer(t1.getText());
+
+/**
+ *  StringBuffer>>>>>TextArea
+ */
+StringBuffer strbf2=new StringBuffer();
+strbf2=new StringBuffer("hello");
+t1.setText(strbf2.toString());
+
