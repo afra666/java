4e4d851012a4c79e9480b5745186f10a6d743298
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-04 22:10:41 +0800

    Update 029.BigDecimal矩阵乘法.java

--- 029.BigDecimal矩阵乘法.java
+++ 029.BigDecimal矩阵乘法.java
@@ -1,4 +1,82 @@
 /**
+ * BigDecimal矩阵乘法函数调用
+ */
+package p;
+import java.math.BigDecimal;
+public class Main {
+	public static void main(String[] args) {
+		int arintA[][]={{1,2},{3,4},{5,6}};
+		int arintB[][]={{1,2,3,4},{4,5,6,7}};
+		BigDecimal[][]arA=new BigDecimal[arintA.length][arintA[0].length];//自己定义矩阵
+		BigDecimal[][]arB=new BigDecimal[arintB.length][arintB[0].length];//自己定义矩阵
+		for (int i = 0; i <arintA.length; i++) {
+			for (int j = 0; j <arintA[0].length; j++) {
+				arA[i][j]=BigDecimal.valueOf(arintA[i][j]);
+			}
+		}
+		for (int i = 0; i < arB.length; i++) {
+			for (int j = 0; j <arintB[0].length; j++) {
+				arB[i][j]=BigDecimal.valueOf(arintB[i][j]);
+			}
+		}
+		/**
+		 * 以上赋初值
+		 */
+		/**
+		 * 调用矩阵乘法函数
+		 */
+		BigDecimal[][]arC=new BigDecimal[arA.length][arB[0].length];
+		arC=matrixMult(arA, arB);
+		for (int i = 0; i < arC.length; i++) {
+			for (int j = 0; j < arC[0].length; j++) {
+				System.out.print(arC[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+	/**
+	 * Decimal矩阵乘法
+	 * @param arA
+	 * @param arB
+	 * @return
+	 */
+	public static BigDecimal [][]matrixMult(BigDecimal [][]arA,BigDecimal [][]arB) {
+		BigDecimal[][]arC=new BigDecimal[arA.length][arB[0].length];
+		//注意删除这个for会出错
+		for (int i = 0; i < arA.length; i++) {
+			for (int j = 0; j < arB[0].length; j++) {
+				arC[i][j]=new BigDecimal("0");
+			}
+		}
+		int rowA=arA.length,coluA=arA[0].length,rowB=arB.length,coluB=arB[0].length;
+		for (int i = 0; i <rowA; i++) {//按照A的行运算
+			for (int j = 0; j <coluB; j++) {//按照B的列运算
+				for (int k = 0; k <arB.length; k++) {//b有多少行，kmax即sigma的顶端就是几
+				arC[i][j]=arC[i][j].add(arA[i][k].multiply(arB[k][j]));
+			}
+				
+		}
+	}
+		
+		return arC;
+		
+	}
+	/**
+	 * 函数结束
+	 */
+}
+/**
+ * 输出
+9 12 15 18 
+19 26 33 40 
+29 40 51 62 
+*/
+
+
+
+
+/**
  * BigDecimal矩阵乘法
  */
 package p;
