23dea8b56083ca3b1c2b744eb6f45a81c380330d
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-05 18:35:56 +0800

    Create 034..java

---
+++ 034..java
@@ -0,0 +1,18 @@
+/**无参数构造函数
+ * 名称要相同
+ */
+package p;
+public class TestClass {
+	TestClass() {
+        System.out.println("Bike is created");
+    }
+
+    public static void main(String args[]) {
+        TestClass b = new TestClass();
+    }
+
+}
+/**
+Bike is created
+
+*/
