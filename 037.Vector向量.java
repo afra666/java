b52e2060953fffc63be56b083e78d24a4b303ec3
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-05 20:41:39 +0800

    Create 037.Vector向量.java

---
+++ 037.Vector向量.java
@@ -0,0 +1,36 @@
+package p;
+import java.util.Vector;
+public class TestClass {
+ 
+   public static void main(String args[]) {
+   Vector<String> v=new Vector<String>(); 
+    System.out.println(v);
+	v.addElement("one"); 
+	System.out.println(v);
+	v.addElement("two"); 
+	System.out.println(v);
+	v.addElement("three"); 
+	System.out.println(v);
+	v.insertElementAt("zero",0); 
+	System.out.println(v);
+	v.insertElementAt("oop",3); 
+	System.out.println(v);
+	v.setElementAt("three",3); 
+	System.out.println(v);
+	v.setElementAt("four",4); 
+	System.out.println(v);
+	v.removeAllElements(); 
+	System.out.println(v);
+   }
+}
+/**
+[]
+[one]
+[one, two]
+[one, two, three]
+[zero, one, two, three]
+[zero, one, two, oop, three]
+[zero, one, two, three, three]
+[zero, one, two, three, four]
+[]
+ */	
