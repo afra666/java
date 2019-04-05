ddfd6f2a2b6d2446838515a225978af68717a418
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-05 19:38:09 +0800

    Create 036.compareTo方法.java

---
+++ 036.compareTo方法.java
@@ -0,0 +1,23 @@
+/**
+ * compareTo()方法
+ */
+
+package p;
+public class TestClass {
+	public static void main(String[] args) {
+		String str1="ab";
+		String str2="a";
+		String str3="236555";
+		String str4="23";
+		int a,b,c;
+		a=str1.compareTo(str2);
+		b=str3.compareTo(str4);
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
+/**
+1
+4
+ */
