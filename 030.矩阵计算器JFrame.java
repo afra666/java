7f45e705407e87523754d0ad9d6151db6266e9a7
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-05 10:56:50 +0800

    Update 030.矩阵计算器JFrame.java

--- 030.矩阵计算器JFrame.java
+++ 030.矩阵计算器JFrame.java
@@ -17,7 +17,7 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
-public class JFrame040401 extends JFrame {
+public class JFrame040407 extends JFrame {
 	boolean opbool=true;
 	private JPanel contentPane;
 	int lineA=0,lineB=0,coluA=0,coluB=0;
@@ -28,7 +28,7 @@ public class JFrame040401 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					JFrame040401 frame = new JFrame040401();
+					JFrame040407 frame = new JFrame040407();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,29 +39,56 @@ public class JFrame040401 extends JFrame {
 	
 	public static BigDecimal [][]matrixMult(BigDecimal [][]arA,BigDecimal [][]arB) {
 		BigDecimal[][]arC=new BigDecimal[arA.length][arB[0].length];
-		//注意删除这个for会出错
-		for (int i = 0; i < arA.length; i++) {
-			for (int j = 0; j < arB[0].length; j++) {
-				arC[i][j]=new BigDecimal("0");
-			}
-		}
-		int rowA=arA.length,coluA=arA[0].length,rowB=arB.length,coluB=arB[0].length;
-		for (int i = 0; i <rowA; i++) {//按照A的行运算
-			for (int j = 0; j <coluB; j++) {//按照B的列运算
-				for (int k = 0; k <arB.length; k++) {//b有多少行，kmax即sigma的顶端就是几
-				arC[i][j]=arC[i][j].add(arA[i][k].multiply(arB[k][j]));
+			if (arA[0].length==arB.length) {
+				//注意删除这个for会出错
+				for (int i = 0; i < arA.length; i++) {
+					for (int j = 0; j < arB[0].length; j++) {
+						arC[i][j]=new BigDecimal("0");
+					}
+				}
+				int rowA=arA.length,coluA=arA[0].length,rowB=arB.length,coluB=arB[0].length;
+				for (int i = 0; i <rowA; i++) {//按照A的行运算
+					for (int j = 0; j <coluB; j++) {//按照B的列运算
+						for (int k = 0; k <arB.length; k++) {//b有多少行，kmax即sigma的顶端就是几
+						arC[i][j]=arC[i][j].add(arA[i][k].multiply(arB[k][j]));
+					}
+						
+				}
 			}
 				
-		}
-	}
+				
+			}
+			return arC;
+			
 		
-		return arC;
+	}
+	public static BigDecimal [][]matrixAdd(BigDecimal [][]arA,BigDecimal [][]arB) {
+		
+		if ((arA.length==arB.length)&&(arA[0].length==arB[0].length)) {
+			BigDecimal xyBigD[][]=new BigDecimal[arA.length][arA[0].length];
+			for (int i = 0; i < arA.length; i++) {
+				for (int j = 0; j < arA[0].length; j++) {
+					xyBigD[i][j]=new BigDecimal("0");
+					xyBigD[i][j]=arA[i][j].add(arB[i][j]);
+				}
+			}
+			return xyBigD;
+		}
+		else {
+			return null;
+		}
+		
+		
+		
+		
+		
+		
 		
 	}
 	/**
 	 * Create the frame.
 	 */
-	public JFrame040401() {
+	public JFrame040407() {
 		setTitle("\u77E9\u9635\u8BA1\u7B97\u5668");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 634, 341);
@@ -81,7 +108,7 @@ public class JFrame040401 extends JFrame {
 		
 		JTextArea t1 = new JTextArea();
 		t1.setFont(new Font("SansSerif", Font.PLAIN, 20));
-		t1.setText("1 2\n3 4\n5 6");
+		t1.setText("");
 		SP1.setViewportView(t1);
 		
 		JScrollPane SP2 = new JScrollPane();
@@ -90,7 +117,7 @@ public class JFrame040401 extends JFrame {
 		
 		JTextArea t2 = new JTextArea();
 		t2.setFont(new Font("SansSerif", Font.PLAIN, 20));
-		t2.setText("4 6 1\n2 5 1");
+		t2.setText("");
 		SP2.setViewportView(t2);
 		
 		JScrollPane SP3 = new JScrollPane();
@@ -99,7 +126,7 @@ public class JFrame040401 extends JFrame {
 		
 		JTextArea t3 = new JTextArea();
 		t3.setFont(new Font("SansSerif", Font.PLAIN, 20));
-		t3.setText("1");
+		t3.setText("");
 		SP3.setViewportView(t3);
 		
 		JButton eqbtn = new JButton("=");
@@ -201,6 +228,7 @@ public class JFrame040401 extends JFrame {
 				BigDecimal xyBigA[][]=new BigDecimal[lineA][coluA];
 				BigDecimal xyBigB[][]=new BigDecimal[lineB][coluB];
 				BigDecimal xyBigC[][]=new BigDecimal[lineA][coluB];
+				BigDecimal xyBigD[][]=new BigDecimal[lineA][coluA];
 				for (int i = 0; i < lineA; i++) {
 					for (int j = 0; j < coluA; j++) {
 						xyBigA[i][j]=new BigDecimal(tokenA.nextElement().toString());
@@ -220,21 +248,50 @@ public class JFrame040401 extends JFrame {
 				 * BigDecimal xyBigA[][]=new BigDecimal[lineA][coluA]
 				 * BigDecimal xyBigB[][]=new BigDecimal[lineB][coluB]
 				 */
-				xyBigC=matrixMult(xyBigA, xyBigB);
-				for (int i = 0; i < xyBigA.length; i++) {
-					for (int j = 0; j < xyBigB[0].length; j++) {
-						if ((i==0)&&(j==0)) {
-							t3.setText(xyBigC[0][0].toString()+" ");
+				if (opbool==true) {
+					try {
+						xyBigC=matrixMult(xyBigA, xyBigB);
+						for (int i = 0; i < xyBigA.length; i++) {
+							for (int j = 0; j < xyBigB[0].length; j++) {
+								if ((i==0)&&(j==0)) {
+									t3.setText(xyBigC[0][0].toString()+" ");
+								}
+								else {
+									t3.setText(t3.getText()+xyBigC[i][j].toString()+" ");
+								}
+								
+								System.out.print(xyBigC[i][j]+" ");
+							}
+							t3.setText(t3.getText()+"\n");
+							System.out.println();
 						}
-						else {
-							t3.setText(t3.getText()+xyBigC[i][j].toString()+" ");
-						}
-						
-						System.out.print(xyBigC[i][j]+" ");
+					} catch (Exception e) {
+						t3.setText("输入矩阵有误或有空行");
 					}
-					t3.setText(t3.getText()+"\n");
-					System.out.println();
 				}
+				else if (opbool==false) {
+					try {
+						xyBigD=matrixAdd(xyBigA,xyBigB);
+						for (int i = 0; i < xyBigD.length; i++) {
+							for (int j = 0; j < xyBigD[0].length; j++) {
+								if ((i==0)&&(j==0)) {
+									t3.setText(xyBigD[i][j].toString()+" ");
+								}
+								else {
+									t3.setText(t3.getText()+xyBigD[i][j].toString()+" ");
+								}
+							}
+							t3.setText(t3.getText()+"\n");
+						}
+					} catch (Exception e) {
+						t3.setText("输入矩阵有误或有空行");
+					}
+				}
+				
+				
+				
+				
+				
 			}
 		});
 		
@@ -248,9 +305,9 @@ t1输入：
 5 6
 t2输入：
 4 6 1
-2 5 1
-t3输出：
-8 16 3 
-20 38 7 
-32 60 11 
+2 5 1.6
+t3乘法输出：
+8 16 4.2 
+20 38 9.4 
+32 60 14.6  
 */
