9c2469bf090b21cbab87c40b44d14d9beb2ae824
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-06 09:17:42 +0800

    Update 040.Iterator和Vector.java

--- 040.Iterator和Vector.java
+++ 040.Iterator和Vector.java
@@ -1,5 +1,34 @@
 /**
- * Iterator
+ * 搜索并删除madehua,正确写法1,使用hasNext
+ */
+package p;
+import java.util.Iterator;
+import java.util.Vector;
+public class TestClass {
+	public static void main(String args[]) {
+		Vector<String> v=new Vector<String>();
+		Vector<String> v2=new Vector<String>();
+		v.add("liudehua");
+		v.add("madehua");
+		v.add("liushishi");
+		v.add("tangwei");
+		Iterator<String> iter=v.iterator();
+		while(iter.hasNext()) { 			//大多情况会使用.hasNext
+			if (iter.next()=="madehua") {
+				iter.remove();
+			}
+		}
+		System.out.println(v);
+	}
+}
+/**
+[liudehua, liushishi, tangwei]
+ */
+
+
+
+/**@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ * Iterator搜索并删除madehua正确写法2
  * 
  */
 
@@ -25,6 +54,56 @@ public class TestMain{
 	 }
 }
 
-/**
+/**@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 [e, a, d]
 */
+/**
+ * 搜索并删除madehua,正确写法3
+ */
+package p;
+import java.util.Iterator;
+import java.util.Vector;
+public class TestClass {
+	public static void main(String args[]) {
+		Vector<String> v=new Vector<String>();
+		v.add("liudehua");
+		v.add("madehua");
+		v.add("liushishi");
+		v.add("tangwei");
+		
+		for (Iterator<String> iter=v.iterator();iter.hasNext();) {
+			String s=iter.next();
+			if (s.equals("madehua")) {
+				iter.remove();
+			}
+		}
+		System.out.println(v);
+	}
+}
+/**
+[liudehua, liushishi, tangwei]
+ */
+
+
+！/**@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+！ * 搜索并删除madehua,错误写法
+！ */
+！//package p;
+！//import java.util.Iterator;
+！//import java.util.Vector;
+！//public class TestClass {
+！//   public static void main(String args[]) {
+！//	Vector<String> v=new Vector<String>();
+！//	v.add("liudehua");
+！//	v.add("madehua");
+！//	v.add("liushishi");
+！//	v.add("tangwei");
+！//	for (Object s:v) {
+！//		if (s=="madehua") {
+！//			v.remove("madehua");
+！//		}
+！//	}
+！//	System.out.println("madehua");
+！//   }
+！//}
+！//
