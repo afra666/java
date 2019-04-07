f92d7e345b3bd3355b6f1bd0d0d7ad41370fb8a0
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-07 20:01:32 +0800

    Update 049.中缀转后缀表达式.java

--- 049.中缀转后缀表达式.java
+++ 049.中缀转后缀表达式.java
@@ -1,60 +1,35 @@
-/**
- * 中缀转后缀
- */
 package p2;
-
 import java.util.Stack;
 import java.util.StringTokenizer;
-
 public class C2 {
-
 	public static void main(String[] args) {
-		String s="5 + ((1 + 2) * 4) - 3";
-		String []input=new MathFormat().valueOf(s);
-		System.out.println(input.length);
-		Stack<String> output=new RPN().result(input);
-		System.out.println(output.size());//output.size()会变，不能用于for循环
-		System.out.println();
+		String []input=new MathFormat().valueOf("5 + ((1 + 2) * 4) - 3");
+		Stack<String> output=new RPN(input).numStk;
 		int outsize=output.size();
-		
 		for (int i = 0; i < outsize; i++) {
 			System.out.println(output.pop());
 		}
-
-
-
-		
 	}
 }
-
 class RPN{
-	/**输入：String数组
-	 * 	输出：
-	 */
-	
-//	Stack<String> outputStk=new Stack<String>();
-	String temp;
 	Stack<String> numStk=new Stack<String>();
 	Stack<String> opStk=new Stack<String>();
-	Stack<String> result(String []input){
+	RPN(String []input){
 		for (int i = 0; i < input.length; i++) {
-				switch (input[i]) {                  
-				case "+":popJudge(input[i],1);break; 
-				case "-":popJudge(input[i],1);break; 
-				case "*":popJudge(input[i],2);break; 
-				case "/":popJudge(input[i],2);break; 
-				case "(":opStk.push(input[i]);break; //左括号直接push
-				case ")":
-					while (!opStk.isEmpty()) {//处理右括号，一直pop到顶部为左括号为止
-						if (opStk.peek().equals("(")) {
-							temp=opStk.pop();break;
-							}
-						else {numStk.push(opStk.pop());}
-					}
-					break; 
-				default:numStk.push(input[i]);break; 
-				}//switch
-				
+			switch (input[i]) {                  
+			case "+":popJudge(input[i],1);break; 
+			case "-":popJudge(input[i],1);break; 
+			case "*":popJudge(input[i],2);break; 
+			case "/":popJudge(input[i],2);break; 
+			case "(":opStk.push(input[i]);break; //左括号直接push
+			case ")":
+				while (!opStk.isEmpty()) {//处理右括号，一直pop到顶部为左括号为止
+					if (opStk.peek().equals("(")) {opStk.pop();break;}
+					else {numStk.push(opStk.pop());}
+				}
+				break; 
+			default:numStk.push(input[i]);break; 
+			}//switch
 		}
 		while (!opStk.isEmpty()) {
 			if (opStk.peek()=="(") {
@@ -62,7 +37,7 @@ class RPN{
 			}
 			numStk.push(opStk.pop());
 		}
-		return numStk;
+
 	}
 	void popJudge(String inputi,int isPri) {
 		if (isPri==1) {
@@ -79,7 +54,7 @@ class RPN{
 				}
 				opStk.push(inputi);//容易丢
 			}
-			
+
 		}
 		else if (isPri==2) {
 			if (opStk.isEmpty()||(priJudge(opStk.peek())==100)) {
@@ -91,13 +66,10 @@ class RPN{
 			else if (priJudge(opStk.peek())==2) {
 				numStk.push(opStk.pop());
 				opStk.push(inputi);
-				
-				
 			}
-			
+
 		}
 	}	
-	
 	int priJudge(String s) {
 		switch (s) {
 		case ("+"):return 1;
@@ -105,16 +77,13 @@ class RPN{
 		case ("*"):return 2;
 		case ("/"):return 2;
 		case ("("):return 100;
-
 		default:return -1;
 		}
 	}
 }
-
 class MathFormat {
 	String  []valueOf(String str1) {
 		String str2;
-
 		str2=str1.replace("+", " + ");
 		str2=str2.replace("-", " - ");
 		str2=str2.replace("*", " * ");
@@ -129,11 +98,15 @@ class MathFormat {
 		return strAr3;
 	}
 }
+
+/**
+ * RPN逆波兰表达式转换
+ */
+/**输入：String[]数组
+ * 输出：栈
+ */
 /**
  * 输出*
-13
-9
-
 -
 3
 +
