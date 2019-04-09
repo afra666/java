1557fe3c16bf948320995b30b3a5855b046968e1
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-09 20:00:58 +0800

    Update 066.Lambda表达式函数式编程.java

--- 066.Lambda表达式函数式编程.java
+++ 066.Lambda表达式函数式编程.java
@@ -1,14 +1,25 @@
 package p;
-public class TestMain {
+/**effect:
+ *input:
 
-	static Thread thread = new Thread(() -> System.out.println("In another thread"));
-	public static void main(String[] args) {
+ *output:
+
+ *start
+ */
+
+public class TestMain{
+	static Thread thread=new Thread(()->System.out.println("Hello world ! From another thread"));
+	public static void main(String []args){
 		thread.start();
-		System.out.println("In main");
+		System.out.println("This is from main");
 	}
 }
-/**輸出
-In main
-In another thread
+/**effect:
+ *input:
 
-*/
+ *output:
+This is from main
+Hello world ! From another thread
+
+ *end
+ */
