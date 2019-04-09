3aa14683771588eae7c47ce14863667a3b0ec546
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-09 20:17:45 +0800

    Create 067.Function用法复合函数.java

---
+++ 067.Function用法复合函数.java
@@ -0,0 +1,27 @@
+package p;
+
+import java.util.function.Function;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Function<Double, Double> fx = x1 -> x1+1.0;
+		Function<Double, Double> gx = x2 -> x2*2.0;
+		Double x=2.0;
+		System.out.println("f(x)=x+1,when x=" + x + ", f(x)=" + fx.apply(x));
+		System.out.println("f(x)=x+1,g(x)=2x, when x=" + x + ", f(g(x))=" + fx.compose(gx).apply(x));
+		System.out.println("f(x)=x+1,g(x)=2x, when x=" + x + ", g(f(x))=" + fx.andThen(gx).apply(x));
+		System.out.println("compose vs andThen:f(g(x))=" +
+				fx.compose(gx).apply(x) + "," + gx.andThen(fx).apply(x));
+	}
+}
+/**effect:
+ *input:
+
+ *output:
+f(x)=x+1,when x=2.0, f(x)=3.0
+f(x)=x+1,g(x)=2x, when x=2.0, f(g(x))=5.0
+f(x)=x+1,g(x)=2x, when x=2.0, g(f(x))=6.0
+compose vs andThen:f(g(x))=5.0,5.0
+
+ *end
+ */
