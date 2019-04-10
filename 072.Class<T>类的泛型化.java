725ecce4bc945fa41e9ad39c57ac959dbf28da82
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-10 11:18:08 +0800

    Create 072.Class<T>类的泛型化.java

---
+++ 072.Class<T>类的泛型化.java
@@ -0,0 +1,13 @@
+package p;
+
+public class TestMain {
+	static Class<TestMain> TYPE=TestMain.class;
+	public static void main(String[] a) {
+		System.out.println(TYPE);
+
+	}
+}
+/**
+ 输出*
+class p.TestMain
+ */
