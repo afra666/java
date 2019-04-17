11dd63a44367811f63ad347ac5f0e4a04a7220ef
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-17 17:04:23 +0800

    Update 092.伪class.getResource获取相对路径.java

--- 092.伪class.getResource获取相对路径.java
+++ 092.伪class.getResource获取相对路径.java
@@ -1,3 +1,11 @@
+/**
+|src--------
+    |com--------
+        |game--------
+            |App.java
+            |img
+*/
+
 
 URL	pigLeftPath;
 pigInit("img/pig-left.png","img/pig-up.png","img/pig-right.png","img/pig-down.png",5);
