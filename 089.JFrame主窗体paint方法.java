1fec79e5a69a6b35b637132ac6c6b80732f8229a
chooseit <48674984+chooseit@users.noreply.github.com>
2019-04-17 11:12:45 +0800

    Update 089.JFrame主窗体paint方法.java

--- 089.JFrame主窗体paint方法.java
+++ 089.JFrame主窗体paint方法.java
@@ -8,17 +8,26 @@ import javax.swing.JFrame;
 
 public class Shape extends JFrame{
 	Shape(){
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(800, 600);
-		this.setLocationRelativeTo(null);
+		this.setTitle("this");
+		this.setBounds(300, 200, 1000, 800);
 		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JFrame jframe1=new JFrame("jframe1");
+		jframe1.setBounds(400, 400, 400, 200);
+		jframe1.setVisible(true);
+		jframe1.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+
 	}
+	
 	@Override
 	public void paint(Graphics g) {
-		
 		g.drawLine(100, 100, 200, 200);
 		g.drawRect(100, 100, 100, 100);
 	}
+	
+	
 	public static void main(String []args){
 		new Shape();
 	}
