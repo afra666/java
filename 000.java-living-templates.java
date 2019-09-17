f46120d8e3d681fac726b4d985befcef1cccfb35
afra666 <48674984+afra666@users.noreply.github.com>
2019-09-17 11:24:43 +0800

    Create 000.java-living-templates.java

---
+++ 000.java-living-templates.java
@@ -0,0 +1,84 @@
+=========intellij===========
+main 
+
+package & MainClass 
+
+package p;
+public class Main {    
+public static void main(String[] args) {        
+System.out.println("$END$");    
+  }
+}
+====================
+main     
+
+void main(String[] args)
+
+public static void main(String[] args) {        
+System.out.println("$END$");    
+}
+====================
+syso 
+
+System.out.println("$END$"); 
+
+System.out.println("$END$");
+====================
+str
+
+String
+
+String
+
+====================
+note1
+
+ntoe 3 lines
+
+/**
+$END$
+**/
+====================
+note1
+
+ntoe 1 lines
+
+/**$END$**/
+====================================================================================================================
+do
+
+do while
+
+do {
+	$END$
+} while ();
+====================
+else
+
+else {
+	$END$
+}
+
+else {
+	$END$
+}
+====================
+elseif
+
+else if ($END$) {
+	$END$
+}
+====================
+finally
+
+finally {
+	$END$
+}
+====================
+for
+for(int i=0;i<?;i++)
+for (int ${index} = 0; ${index} < ${array}.length; ${index}++) {
+	$END$
+}
+====================
+
