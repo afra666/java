303b3f1cf2c0c766d1f2834288a825d209dd7960
afra666 <48674984+afra666@users.noreply.github.com>
2019-11-26 20:39:47 +0800

    Create 102.字符串比对.java

---
+++ 102.字符串比对.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String str1="Hello world!";
+        String str2="Hello world!";
+        if (str1.equals(str2)){
+            System.out.println("相等");
+        }else{
+            System.out.println("不相等");
+        }
+    }
+}
